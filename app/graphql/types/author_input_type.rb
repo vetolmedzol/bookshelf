ace7079fe356f4ad07e32f221fc55e1cfd8a9ba3
vetolmedzol <vetolmedzol@gmail.com>
2022-02-15 10:48:26 +0200

    added create author mutation

---
+++ app/graphql/types/author_input_type.rb
@@ -0,0 +1,12 @@
+module Types
+  class AuthorInputType < GraphQL::Schema::InputObject
+    graphql_name 'AuthorInputType'
+    description 'All the attributes needed to create an author'
+
+    argument :first_name, String, required: false, camelize: false
+    argument :last_name, String, required: false, camelize: false
+    argument :year_of_birth, Int, required: false, camelize: false
+    argument :is_alive, Boolean, required: false, camelize: false
+
+  end
+end
